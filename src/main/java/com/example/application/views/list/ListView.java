5a1944d18501143ced248fb5847868af5a651490
Nuttasit <beginnew@hotmail.com>
2023-12-21 13:06:15 +0700

    vaadin tutorial

--- src/main/java/com/example/application/views/list/ListView.java
+++ src/main/java/com/example/application/views/list/ListView.java
@@ -1,9 +1,12 @@
 package com.example.application.views.list;
 
+import com.example.application.data.Contact;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
@@ -11,23 +14,18 @@ import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
 @PageTitle("list")
 @Route(value = "")
 public class ListView extends VerticalLayout {
-
+    Grid<Contact> grid = new Grid<>(Contact.class);
+    TextField filterText = new TextField();
     public ListView() {
-        setSpacing(false);
-
-        Image img = new Image("images/empty-plant.png", "placeholder plant");
-        img.setWidth("200px");
-        add(img);
-
-        H2 header = new H2("This place intentionally left empty");
-        header.addClassNames(Margin.Top.XLARGE, Margin.Bottom.MEDIUM);
-        add(header);
-        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
-
+        addClassName("list-view");
         setSizeFull();
-        setJustifyContentMode(JustifyContentMode.CENTER);
-        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-        getStyle().set("text-align", "center");
+
+        configureGrid();
+    }
+
+    private void configureGrid() {
+        grid.addClassName("contact-grid");
+        grid.setSizeFull();
     }
 
 }
